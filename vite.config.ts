4ace2bcd55bb00e4e568fbb452a1ae5109a2a46b
Trevor Smith <trevorsm@pm.me>
2025-10-23 12:33:27 -0600

    gh deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // use docs for build instead of dist
-  build: {
-    outDir: "docs",
-  },
+  base: "https://trevsm.github.io/particlelife-sim/",
 })
